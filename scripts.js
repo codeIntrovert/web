e3b0ecb449e263838dabe32e7203fc56cc0ca357
Hasan <46672438+codeIntrovert@users.noreply.github.com>
2022-08-04 18:30:53 +0530

    major over haul alphavibe

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function hasan()
+{
+    document.getElementById("body").style.backgroundColor="black";
+    document.getElementById("welcome").style.color="white";
+    var myMusic= document.getElementById("music");
+    {
+        myMusic.play();
+    }
+}
