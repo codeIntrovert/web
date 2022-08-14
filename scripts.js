ec2002b82d6c92a32b71f74001053a38e3b7ea7a
Hasan <46672438+codeIntrovert@users.noreply.github.com>
2022-08-14 15:52:28 +0530

    alphavibe complete

---
+++ scripts.js
@@ -0,0 +1,47 @@
+function heart()
+{
+    var myMusic= document.getElementById("heart");
+    {
+        myMusic.play();
+    }
+}
+function charge()
+{
+    var myMusic= document.getElementById("charge");
+    {
+        myMusic.play();
+    }
+}
+function bandage()
+{
+    var myMusic= document.getElementById("bandage");
+    {
+        myMusic.play();
+    }
+}
+function boat()
+{
+    var myMusic= document.getElementById("boat");
+    {
+        myMusic.play();
+    }
+}
+function stop()
+{
+    var myMusic= document.getElementById("charge");
+    {
+        myMusic.pause();
+    }
+    var myMusic= document.getElementById("heart");
+    {
+        myMusic.pause();
+    }    
+    var myMusic= document.getElementById("bandage");
+    {
+        myMusic.pause();
+    }    
+    var myMusic= document.getElementById("boat");
+    {
+        myMusic.pause();
+    }
+}
